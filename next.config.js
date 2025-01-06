db19c137d49f716586ce86c6e338afec9fd3f171
mostafa <mostafaakajdid6@gmail.com>
2025-01-06 10:06:23 +0100

    mos

--- next.config.js
+++ next.config.js
@@ -1,20 +1,31 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: ['images.unsplash.com', 'firebasestorage.googleapis.com'], // Add all required domains here
-//   },
-// };
-
-// module.exports = nextConfig;
-// const createNextIntlPlugin = require('next-intl/plugin');
- 
-// const withNextIntl = createNextIntlPlugin();
- 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'firebasestorage.googleapis.com'], // Add all required domains here
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Explicitly set the allowed origin
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { 
+            key: "Access-Control-Allow-Headers", 
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" 
+          },
+        ],
+      },
+    ];
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
+
+// Export the configuration
+module.exports = nextConfig;
